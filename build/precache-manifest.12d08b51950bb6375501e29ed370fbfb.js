e13162ace749eaa12a3af744991f157a015384bc
Kushagra <kushagramandal01@gmail.com>
2022-07-29 22:22:54 +0530

    added build

---
+++ build/precache-manifest.12d08b51950bb6375501e29ed370fbfb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e99504b35de7d8aef8cc89d9b05b79a4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5c9f17dbd43a0f9975d6",
+    "url": "/static/css/main.e70b4508.chunk.css"
+  },
+  {
+    "revision": "7510614ae394d5f34634",
+    "url": "/static/js/2.4aadedc0.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/static/js/2.4aadedc0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5c9f17dbd43a0f9975d6",
+    "url": "/static/js/main.b0e5f70f.chunk.js"
+  },
+  {
+    "revision": "97c002b8f8a25334b102",
+    "url": "/static/js/runtime-main.67d84520.js"
+  }
+]);
